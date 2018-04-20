4f5eec82aa2baa64c99e5d9244b300392e52ff21
Ryosuke Ohta <mailryosuke@gmail.com>
2018-04-20 23:38:08 +0900

    add optin check.

--- option.js
+++ option.js
@@ -1,22 +1,49 @@
-(function(){
+( () => {
+  let conf = {
+    "defailt":{
+      "google":"1",
+      "yahoo":"2",
+      "dictionary.cambridge.org":"3"
+    }
+  };
+
   document.querySelector('#optionForm').addEventListener('submit',resistFormOption);
 
   function resistFormOption(e){
+    console.log(e);
     e.preventDefault();
-    console.log("resistFormOption");
-    //console.log(e);
-    //console.log(e.target);
 
     let form = e.target;
-    let input = {};
-    let registers = form.querySelectorAll("[name=register]");
-    console.log(registers);
-
-    //iterable...
+    let registers = form.querySelectorAll("input[name=register]:checked");
 
     for(let i=0; i<registers.length; i++){
       console.log("i="+i);
+      let value = registers[i].value;
+      console.log("value="+value);
+
+      browser.contextMenus.create(
+        {
+          "id":value,
+          "title":value,
+          "contexts":["selection"]
+        },
+        onCreated
+      );
     }
-    //browser.storage.local.get();
+
+    function onCreated(e){
+      console.log("e="+e);
+    }
+
+    browser.contextMenus.onClicked.addListener(function(info, tab) {
+      console.log(info);
+      console.log(tab);
+      switch (info.menuItemId) {
+        case "log-selection":
+          console.log(info.selectionText);
+          break;
+      }
+    });
+
   }
 })();
