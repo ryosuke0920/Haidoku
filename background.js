70efbc17f57dadaecdbf10200035e5eb5ea75db3
Ryosuke Ohta <mailryosuke@gmail.com>
2018-04-22 18:06:18 +0900

    combine option and background.

--- background.js
+++ background.js
@@ -1,43 +1,73 @@
-(function(){
-  let config = {
-    options: [
-      {
-        id: "AddingURL2ContextMenu-id-1",
-        title: "search by dictionary.cambridge.org",
-        contexts: ["selection"],
-        url: "https://dictionary.cambridge.org/search/english/direct/?q=$1"
-      },
-      {
-        id: "AddingURL2ContextMenu-id-2",
-        title: "search by another site.",
-        contexts: ["selection"],
-        url: "https://www.yahoo.co.jp/$1"
-      }
-    ]
-  };
+( () => {
+	browser.storage.onChanged.addListener(resetting);
 
-  for(let i=0; i<config["options"].length; i++){
-    let item = config.options[i];
-    browser.contextMenus.create({
-      id: item["id"],
-      title: item["title"],
-      contexts: item["contexts"]
-    });
-  }
+	function resetting(){
+		console.log("resetting");
+		browser.contextMenus.removeAll();
+		let getter = browser.storage.local.get({
+			"registors": []
+		});
+		getter.then(onGot, onError);
 
-  browser.contextMenus.onClicked.addListener(function(info, tab) {
-    for(let i=0; i<config["options"].length; i++){
-      let item = config.options[i];
-      if( info.menuItemId == item["id"] ) {
-        gotoSite(item["url"],info.selectionText);
-      }
-    }
-  });
+		function onGot(res){
+			console.log("onGot");
+			console.log(res);
+			let registors = res["registors"];
+			console.log(registors);
+			for(let i=0; i<registors.length; i++){
+				let value = registors[i];
+				console.log("value=" + value);
+				let item = options[value];
+				if ( item != null ) {
+					console.log("context menu create.");
+					browser.contextMenus.create({
+						id: item["id"],
+						title: item["title"],
+						contexts: item["contexts"]
+					});
+				}
+			}
+		}
 
-  function gotoSite(url,text){
-    url = url.replace("$1", text);
-    console.log('url='+url);
-    browser.tabs.create({url: url});
-  }
+		function onError(e){
+			console.log("background-onError");
+			console.error(e);
+		}
+	}
 
+	let options = {
+		"www.google.co.jp": {
+			id: "www.google.co.jp",
+			title: "goto www.google.co.jp",
+			contexts: ["selection"],
+			url: "https://www.google.com/search?q=$1"
+		},
+		"www.yahoo.co.jp": {
+			id: "www.yahoo.co.jp",
+			title: "goto www.yahoo.co.jp",
+			contexts: ["selection"],
+			url: "https://search.yahoo.co.jp/?p=$1"
+		},
+		"dictionary.cambridge.org": {
+			id: "dictionary.cambridge.org",
+			title: "goto dictionary.cambridge.org",
+			contexts: ["selection"],
+			url: "https://dictionary.cambridge.org/search/english/direct/?q=$1"
+		}
+	};
+
+	browser.contextMenus.onClicked.addListener( (info, tab) => {
+		console.log("contextMenus.onClicked.addListener");
+		if ( options[info.menuItemId] ){
+			gotoSite( options[info.menuItemId]["url"], info.selectionText );
+		}
+	});
+
+	function gotoSite(url,text){
+		url = url.replace("$1", text);
+		console.log('url='+url);
+		browser.tabs.create({url: url});
+	}
+
+	resetting();
 })();
--- option.js
+++ option.js
@@ -1,77 +1,59 @@
 ( () => {
-
-	let conf = {
-		"defailt":{
-			"www.google.co.jp":"1",
-			"www.yahoo.co.jp":"2",
-			"dictionary.cambridge.org":"3"
-		}
-	};
-
 	document.querySelector('#optionForm').addEventListener('submit',saveOptions);
 	document.addEventListener('DOMContentLoaded', restoreOptions);
 
 	function restoreOptions(e){
-		console.log(e);
-
+		console.log("restoreOptions");
 		let getter = browser.storage.local.get({
 			"registors": []
 		});
 		getter.then(onGot, onError);
 
 		function onGot(res){
-			"use strict";
 			console.log("onGot");
 			console.log(res);
 			let registors = res["registors"];
 			console.log(registors);
 			for(let i=0; i<registors.length; i++){
-				console.log("i="+i);
-
 				let value = registors[i];
 				console.log("value=" + value);
-
 				let select = "input[name=register][value=\"" + value + "\"]";
 				console.log("select="+select);
-
 				let checkbox = document.querySelector(select);
+				console.log(checkbox);
 				checkbox.setAttribute("checked", true);
-				console.log("checkbox="+checkbox);
-
 			}
 		}
+
 		function onError(e){
+			console.log("onError");
 			console.error(e);
-			console.error("getter ng");
 		}
 	}
 
 	function saveOptions(e){
+		console.log("saveOptions");
 		e.preventDefault();
-		console.log(e);
-
 		let form = e.target;
 		let registers = form.querySelectorAll("input[name=register]:checked");
 		let store_registers = [];
-
 		for(let i=0; i<registers.length; i++){
 			let value = registers[i].value;
 			store_registers.push(value);
 		}
-
 		console.log(store_registers);
-
 		let setter = browser.storage.local.set({
 			"registors": store_registers
 		});
-		setter.then((e)=>{ok(e)}, ng);
+		setter.then(onSet, onError);
 
-		function ok(e){
-			console.log("setter ok");
-		}
-		function ng(e){
-			console.error("setter ng");
+		function onSet(){
+			console.log("onSet");
 		}
 
+		function onError(e){
+			console.log("onError");
+			console.error(e);
+		}
 	}
 })();
