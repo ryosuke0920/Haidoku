93798430e4f1bb7ac85464ab2ece44f7e1c0d128
ryosuke <mailryosuke@gmail.com>
2018-06-01 17:44:39 +0900

    resize linkList

--- content.js
+++ content.js
@@ -1,78 +1,101 @@
 (()=>{
-	const DIST = 5;
-	const LINK_NODE_HEIGHT = 100;
-	const LINK_NODE_WIDTH = 200;
+	const LINK_NODE_MIN_HEIGHT = 100;
+	const LINK_NODE_MIN_WIDTH = 200;
+	const LINK_NODE_PADDING = 3;
+	const SPACE = 5;
 	const SCROLL_BAR_WIDTH = 17;
-	const STYLE = "\n\
-* {\n\
-	margin: 0;\n\
-	padding: 0;\n\
-	border: none;\n\
-}\n\
-body {\n\
-	font-size: small;\n\
-	font-family: sans-serif;\n\
-}\n\
-div {\n\
-	background-color: white;\n\
-	box-shadow: rgba(0, 0, 0, 0.32) 0px 2px 2px 0px, rgba(0, 0, 0, 0.16) 0px 0px 0px 1px;\n\
-	margin: 2px;\n\
-	padding: 0.2em;\n\
-	overflow: scroll;\n\
-	white-space: nowrap;\n\
-}\n\
-a {\n\
-	text-decoration: none;\n\
-}\n\
-a:hover {\n\
-	text-decoration: underline;\n\
-}";
 	let px;
 	let py;
 	let cx;
 	let cy;
 	let linkNode;
+	let linkNodeHeight = LINK_NODE_MIN_HEIGHT;
+	let linkNodeWidth = LINK_NODE_MIN_WIDTH;
 	let optionList = [];
 	let linkListFlag = false;
+	let selectStartFlag = false;
+	let resizeFlag = false;
+	let selectedText = "";
+
 	let promise = init();
 
 	function init(){
-		linkNode = document.createElement("iframe");
-		linkNode.scrolling = "no";
-		linkNode.style.position = "absolute";
-		linkNode.style.top = "0px";
-		linkNode.style.left = "0px";
+		linkNode = document.createElement("div");
+		linkNode.style.all = "initial";
+		linkNode.style.margin = "0";
+		linkNode.style.padding = LINK_NODE_PADDING + "px";
 		linkNode.style.display = "none";
+		linkNode.style.position = "absolute";
+		linkNode.style.height = linkNodeHeight + "px";
+		linkNode.style.width = linkNodeWidth + "px";
 		linkNode.style["z-index"] = "2147483646";
-		linkNode.style.border = "none";
-		linkNode.style.height = LINK_NODE_HEIGHT + "px";
-		linkNode.style.width = LINK_NODE_WIDTH + "px";
-		document.querySelector("body").appendChild(linkNode);
+		linkNode.style["background-color"] = "white";
+		linkNode.style["box-shadow"] = "rgba(0, 0, 0, 0.32) 0px 2px 2px 0px, rgba(0, 0, 0, 0.16) 0px 0px 0px 1px";
+		linkNode.style["overflow"] = "auto";
+		linkNode.style["resize"] = "both";
+		document.querySelector("body").appendChild( linkNode );
 		browser.storage.onChanged.addListener( onStorageChanged );
+		let style = document.createElement("style");
+		style.innerText = "\n\
+a.lessLaborGoToDictionary-anchor { \n\
+	all: initial; \n\
+	font-family: sans-serif; \n\
+	font-size: 13px; \n\
+	color: rgb(0, 0, 238); \n\
+	cursor: pointer; \n\
+	white-space: nowrap; \n\
+} \n\
+a.lessLaborGoToDictionary-anchor:visited { \n\
+	color: #551a8b; \n\
+} \n\
+a.lessLaborGoToDictionary-anchor:hover { \n\
+	text-decoration: underline; \n\
+} \n\
+a.lessLaborGoToDictionary-anchor:active { \n\
+	color: #ee0000; \n\
+}";
+		document.querySelector("head").appendChild(style);
 
 		return reload();
 	}
 
 	function addLinkListEvents(){
+		document.addEventListener("selectstart", selectStartBehavior);
+		document.addEventListener("selectionchange", selectionChangeBehavior);
 		document.addEventListener("mouseup", mouseupBehavior);
 		window.addEventListener("resize", resizeBehavior);
 		document.addEventListener("keydown", keydownBehavior);
 		document.addEventListener("mousemove", mousemoveBehavior);
-		document.addEventListener("mousemove", mousemoveBehavior);
-		document.addEventListener("selectionchange", selectionChangeBehavior);
 	}
 
 	function removeLinkListEvents(){
+		document.removeEventListener("selectstart", selectStartBehavior);
+		document.removeEventListener("selectionchange", selectionChangeBehavior);
 		document.removeEventListener("mouseup", mouseupBehavior);
 		window.removeEventListener("resize", resizeBehavior);
 		document.removeEventListener("keydown", keydownBehavior);
 		document.removeEventListener("mousemove", mousemoveBehavior);
-		document.removeEventListener("selectionchange", selectionChangeBehavior);
+	}
+
+	function selectStartBehavior(e) {
+		selectStartFlag = true;
+	}
+
+	function selectionChangeBehavior(e){
+		let selection = window.getSelection();
+		selectedText = selection.toString();
 	}
 
 	function mouseupBehavior(e){
-		if( e.button == "0" ) {
-			linkBehavior(e);
+		if( selectedText.length <= 0 ){
+			closeLinkList();
+			return ;
+		}
+		if( selectStartFlag ){
+			selectStartFlag = false;
+			makeLinkList();
+			showLinkList();
+			return ;
 		}
 	}
 
@@ -81,9 +104,7 @@ a:hover {\n\
 	}
 
 	function keydownBehavior(e){
-		if( e.key == "Escape" || e.key == "Esc") {
-			closeLinkList();
-		}
+		if( e.key == "Escape" || e.key == "Esc") closeLinkList();
 	}
 
 	function mousemoveBehavior(e){
@@ -97,35 +118,41 @@ a:hover {\n\
 		linkNode.style.display = "none";
 	}
 
-	function selectionChangeBehavior(){
-		let selection = window.getSelection();
-		let select = selection.toString();
-		if( linkListFlag && select.length <= 0 ) closeLinkList();
+	function makeLinkList(){
+		while(linkNode.firstChild) linkNode.removeChild(linkNode.firstChild);
+		for(let item of optionList){
+			if ( !item["c"] ) continue;
+			let url = item["u"];
+			url = url.replace( "$1", encodeURIComponent(selectedText) );
+			let a = document.createElement("a");
+			a.classList.add("lessLaborGoToDictionary-anchor");
+			a.setAttribute( "href", url );
+			a.setAttribute( "target", "_blank" );
+			a.innerText = item["l"];
+			linkNode.appendChild(a);
+			let br = document.createElement("br");
+			br.style.all = "initial";
+			linkNode.appendChild(br);
+		}
 	}
 
-	function linkBehavior(){
-		if( linkListFlag ){
-			let selection = window.getSelection();
-			let select = selection.toString();
-			if( select.length > 0 && retrieveLink(select) ) {
-				let yy = window.innerHeight - SCROLL_BAR_WIDTH - ( cy + LINK_NODE_HEIGHT );
-				if ( 0 < yy || window.innerHeight < LINK_NODE_HEIGHT ) yy = 0;
-				let xx = window.innerWidth - SCROLL_BAR_WIDTH - ( cx + LINK_NODE_WIDTH );
-				if ( 0 < xx || window.innerWidth < LINK_NODE_WIDTH ) xx = 0;
-				linkNode.style.top = ( py + yy + DIST )+"px";
-				linkNode.style.left = ( px + xx + DIST ) +"px";
-				linkNode.style.display = "block";
-				return;
-			}
-		}
-		closeLinkList();
+	function showLinkList(){
+		linkNode.style.display = "block"; /* if none, clientHeight and clientWidth return undefined. */
+		let yy = window.innerHeight - cy - linkNode.clientHeight - SCROLL_BAR_WIDTH;
+		if ( 0 < yy || window.innerHeight < linkNode.clientHeight ) yy = 0;
+		let xx = window.innerWidth - cx - linkNode.clientWidth - SCROLL_BAR_WIDTH;
+		if ( 0 < xx || window.innerWidth < linkNode.clientWidth ) xx = 0;
+		let linkNodeTop = py + yy + SPACE;
+		let linkNodeLeft = px + xx + SPACE;
+		linkNode.style.top = linkNodeTop+"px";
+		linkNode.style.left = linkNodeLeft+"px";
 	}
 
 	function onStorageChanged(change, area){
 		closeLinkList();
-		let data = {};
-		if( change["ol"] ) resetOptionList( change["ol"]["newValue"] );
-		if( change["bf"] ) resetLinkListFlag( change["bf"]["newValue"] );
+		if( change["ol"] ) setOptionList( change["ol"]["newValue"] );
+		if( change["bf"] ) setLinkListFlag( change["bf"]["newValue"] );
+		resetLinkListEvents();
 	}
 
 	function reload(){
@@ -134,53 +161,35 @@ a:hover {\n\
 			"bf": false
 		});
 
-		return getter.then(resetVer);
+		return getter.then(setVer);
 	}
 
-	function resetVer( res ){
-		resetOptionList( res["ol"] );
-		resetLinkListFlag( res["bf"] );
+	function setVer( res ){
+		setOptionList( res["ol"] );
+		setLinkListFlag( res["bf"] );
+		resetLinkListEvents();
 	}
 
-	function resetLinkListFlag(res){
+	function setLinkListFlag(res){
 		linkListFlag = res;
-		removeLinkListEvents();
-		if(linkListFlag) addLinkListEvents();
 	}
 
-	function resetOptionList(res){
+	function resetLinkListEvents(){
+		removeLinkListEvents();
+		if( linkListFlag && hasLinkList() ) addLinkListEvents();
+	}
+
+	function setOptionList(res){
 		optionList = [];
 		for( let data of res ){
-			if ( data["c"] ) optionList.push(data);
+			if ( data["c"] ) { /* checked == true */
+				optionList.push(data);
+			}
 		}
 	}
 
-	function retrieveLink(select){
-		if ( optionList.length <= 0 ) return false;
-		let html = document.createElement("html");
-		let head = document.createElement("head");
-		html.appendChild(head);
-		let style = document.createElement("style");
-		style.appendChild(document.createTextNode(STYLE));
-		head.appendChild(style);
-		let body = document.createElement("body");
-		html.appendChild(body);
-		let div = document.createElement("div");
-		div.style.height = ( LINK_NODE_HEIGHT - SCROLL_BAR_WIDTH ) +"px";
-		body.appendChild(div);
-		for(let item of optionList){
-			if ( !item["c"]) continue;
-			let url = item["u"];
-			url = url.replace( "$1", encodeURIComponent(select) );
-			let a = document.createElement("a");
-			a.setAttribute( "href", url );
-			a.setAttribute( "target", "_blank" );
-			a.innerText = item["l"];
-			div.appendChild(a);
-			let br = document.createElement("br");
-			div.appendChild(br);
-		}
-		linkNode.srcdoc = html.outerHTML;
-		return true;
+	function hasLinkList(){
+		if( optionList.length > 0 ) return true;
+		return false;
 	}
 })();
